bbb3659f19692a2935e018468d500e627cbb60ca
KIM YANG MO <yangdol2@nate.com>
2016-04-20 15:18:51 +0900

    .

--- test01/src/Hello.java
+++ test01/src/Hello.java
@@ -6,8 +6,7 @@ public class Hello {
     System.out.println("Hello");
     System.out.println("World");
     System.out.println("Hello World");
-    System.out.println("123123");
-    System.out.println("321321");
+    System.out.println("123123");    
   }
 
 }
